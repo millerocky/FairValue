9c087a24bb8e834a4a4797f3efa9051146e3e863
mille <milleryan2003@gmail.com>
2022-05-13 14:48:32 +0300

    managed the process of web security

--- stocksio/stocksio/settings.py
+++ stocksio/stocksio/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from secret_settings import * 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,8 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uv2=5fzry7byosqr215!4h@qifz=&-_xku#%ma_0cf4x=k5apq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
